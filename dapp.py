679e2c5fe2f7119c266158e1e15ffcef572e7f3d
Milton Jonathan <mjonathan.cartesi@gmail.com>
2025-02-10 18:49:14 -0300

    feat: initial commit performing simple `eval`

---
+++ dapp.py
@@ -0,0 +1,59 @@
+from os import environ
+import logging
+import requests
+
+logging.basicConfig(level="INFO")
+logger = logging.getLogger(__name__)
+
+rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
+logger.info(f"HTTP rollup_server url is {rollup_server}")
+
+
+def emit_notice(data):
+    notice_payload = {"payload": data["payload"]}
+    response = requests.post(rollup_server + "/notice", json=notice_payload)
+    if response.status_code == 200 or response.status_code == 201:
+        logger.info(f"Notice emitted successfully with data: {data}")
+    else:
+        logger.error(f"Failed to emit notice with data: {data}. Status code: {response.status_code}")
+
+def handle_advance(data):
+    logger.info(f"Received advance request data {data}")
+    try:
+        expr_hex = data['payload']
+        expr = bytes.fromhex(expr_hex[2:]).decode('utf-8')
+        print(f"input: {expr}")
+        result = eval(expr)
+        print(f"result: {result}")
+        # TODO: abi-encode result+input
+        result_hex = f"0x{result:064x}"
+        emit_notice({'payload': result_hex})
+        return "accept"
+    
+    except Exception as error:
+        print(f"Error processing payload: {error}")
+        return "reject"
+
+def handle_inspect(data):
+    logger.info(f"Received inspect request data {data}")
+    return "accept"
+
+
+handlers = {
+    "advance_state": handle_advance,
+    "inspect_state": handle_inspect,
+}
+
+finish = {"status": "accept"}
+
+while True:
+    logger.info("Sending finish")
+    response = requests.post(rollup_server + "/finish", json=finish)
+    logger.info(f"Received finish status {response.status_code}")
+    if response.status_code == 202:
+        logger.info("No pending rollup request, trying again")
+    else:
+        rollup_request = response.json()
+        data = rollup_request["data"]
+        handler = handlers[rollup_request["request_type"]]
+        finish["status"] = handler(rollup_request["data"])
